4bff4c4c76f1280788c1cea877801dff6049559c
Hassan-Adel <hasanadel555@hotmail.com>
2017-05-08 21:17:48 +0200

    adding authentication controller and view

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -1,12 +1,18 @@
-export function routerConfig ($stateProvider, $urlRouterProvider) {
-  'ngInject';
-  $stateProvider
-    .state('home', {
-      url: '/',
-      templateUrl: 'app/main/main.html',
-      controller: 'MainController',
-      controllerAs: 'main'
-    });
+export function routerConfig($stateProvider, $urlRouterProvider) {
+    'ngInject';
+    $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: 'app/main/main.html',
+            controller: 'MainController',
+            controllerAs: 'main'
+        })
+        .state('authentication', {
+            url: '/authentication',
+            templateUrl: 'app/authentication/authentication.html',
+            controller: 'AuthenticationController',
+            controllerAs: 'authentication'
+        });
 
-  $urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('/');
 }
